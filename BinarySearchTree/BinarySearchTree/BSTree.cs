9dec0791947c3ab4c00f701e9e04d8664afa0d0b
Sikander <sikku.rk405@gmail.com>
2022-02-15 18:55:40 +0530

    Added Binary search tree program

---
+++ BinarySearchTree/BinarySearchTree/BSTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+
+    class BSTree<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BSTree<T> left { get; set; }
+        public BSTree<T> right{ get; set; }
+
+        public BSTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.right= null;
+            this.left = null;
+
+        }
+        public void Insert(T item)
+        {
+            T nodeValue = this.NodeData;
+            if ((nodeValue.CompareTo(item)) > 0)
+            {
+                if (this.left == null)
+                    this.left = new BSTree<T>(item);
+                else
+                    this.left.Insert(item);
+            }
+            else
+            {
+                if (this.right== null)
+                    this.right= new BSTree<T>(item);
+                else
+                    this.right.Insert(item);
+            }
+        }
+        public void Display()
+        {
+            if (this.left != null)
+            {
+                this.left.Display();
+
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.right!= null)
+            {
+                this.right.Display();
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BSTree<int> Search = new BSTree<int>(56);
+            Search.Insert(30);
+            Search.Insert(70);
+            Search.Display();
+            Console.ReadKey();
+        }
+    }
+}
